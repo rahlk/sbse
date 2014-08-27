a64f5d860747077f0fead9dd35596ef03b949550
Rahul <i.m.ralk@gmail.com>
2014-08-27 03:30:00 -0400

    Version 1

---
+++ hw1_1.py
@@ -0,0 +1,59 @@
+from __future__ import division
+import sys,re,random,math,datetime,re,time
+sys.dont_write_bytecode = True
+
+rand=random.random
+randi=random.randint
+exp=math.exp
+
+def say(x): 
+  sys.stdout.write(str(x)); sys.stdout.flush()
+
+def energy(x):
+  return x**2+(x-2)**2
+
+def neighbour(x):
+  return x+rand()
+
+def pAcceptance(e, en, t):
+  p=exp((e-en)/t)
+  return p
+ 
+# Initial state and energy
+s=randi(-100,100)
+e=energy(s)
+
+# Initial best state and energy
+sb=s;
+eb=e;
+
+k=1 # Initial temperature
+kmax=1000
+emax=4
+
+while k<kmax: 
+  sn=neighbour(s)
+  en=energy(sn)
+  t=k/kmax
+#  print t
+  if en<eb:
+    say('!')
+    sb=sn
+    eb=en
+
+  if en<e:
+    s=sn
+    e=en
+    say('+')
+  
+  elif pAcceptance(e, en, t)>rand():
+    s=sn
+    e=en
+    say('?')
+  
+  say('.')
+  k=k+1
+  if k%50==0: say('\n'),say(sb)
+
+
+
