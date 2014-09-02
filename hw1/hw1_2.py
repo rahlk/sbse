611cddfa0b455c983e72f0778cb595b065995510
Rahul <i.m.ralk@gmail.com>
2014-09-02 15:38:35 -0400

    Update

--- hw1/hw1_2.py
+++ hw1/hw1_2.py
@@ -9,31 +9,28 @@ from __future__ import division
 import sys,re,random,math,datetime,re,time
 sys.dont_write_bytecode = True
 
-rand=random.uniform
+rand=random.random
 randi=random.randint
 exp=math.exp
-random.seed()
 
 class simulatedAnnealing:
 
   def __init__(self):
     pass
   
-  def energy(self,x,emax,emin):
-    f1=x**2
-    f2=(x-2)**2
-    e=f1+f2
-    e_norm=(e-emin)/(14379-emin)
-    #print e_norm
-    return e_norm
+  def energy(self,x):
+    e=x**2+(x-2)**2
+    e_norm=(e)/(23960)
+   # print e_norm
+    return e
 
   def neighbour(self, x, xmin, xmax):
-    x_new=x+xmin+(xmax-xmin)*rand(0,1)
+    x_new=x+xmin+(xmax-xmin)*rand()
     return x_new
 
-  def pAcceptance(self, e, en, t, k):
-    p=exp(-(en-e)/(k*t))
-    print p
+  def pAcceptance(self, e, en, t):
+    p=exp(-(en-e)/t)
+   # print p
     return p
  
   f=open('output.log','w')
@@ -50,26 +47,24 @@ class main:
   
   xmax=100;
   xmin=-100;
-  emax=1;
-  emin=0;
   
   sa=simulatedAnnealing()
 
   # Initial state and energy
   s=randi(-1000,1000)
-  e=sa.energy(s,emax,emin)
-  print e
+  e=sa.energy(s)
+
   # Initial best state and energy
   sb=s;
   eb=e;
  
-  sa.say(sb)
+ # sa.say(sb)
+  emax=e
   
-  while (k<kmax): 
-    sn=sa.neighbour(s,xmax,xmin)
-    en=sa.energy(sn,emax,emin)
+  while (k<kmax):# and e > emax): 
+    sn=sa.neighbour(sb,xmax,xmin)
+    en=sa.energy(sn)
     t=k/kmax
-    
     if en<eb:
       sa.say('!')
       sb=sn
@@ -80,18 +75,14 @@ class main:
       e=en
       sa.say('+')
     
-    elif sa.pAcceptance(e, en, t, 0.01)>rand(0,1):
+    elif sa.pAcceptance(e, en, t)>rand():
       s=sn
       e=en
       sa.say('?')
   
     sa.say('.')
     k=k+1
-    if k%40==0: sa.say('\n'), sa.say(format(sb,'0.2f'))
-  sa.say('\n'),sa.say('Best Value Found '),sa.say(format(sb,'0.2f'))
-  print emax
-  print emin
-  print e
-  print sb
+    if k%50==0: sa.say('\n'), sa.say(sb)
+ # print emax
 if __name__=='main':
   main()
