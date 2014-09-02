4104e8d022d28ab1706a5649a52eee69fc3919fe
Rahul <i.m.ralk@gmail.com>
2014-09-01 20:41:24 -0400

    Final Update

--- hw1/hw1_1.py
+++ hw1/hw1_1.py
@@ -1,79 +1,85 @@
 """
-  _   _   _   _   _   _   _   _   _    
- / \ / \ / \ / \ / \ / \ / \ / \ / \ 
-( S | i | m | u | l | a | t | e | d ) 
- \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ 
- / \ / \ / \ / \ / \ / \ / \ / \ / \ 
-( A | n | n | e | a | l | i | n | g )
- \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/ \_/  
-
 Homework 1 
 
 Authored by Rahul 
-Last updated 08/28/2014
+Last updated September, 1st 2014
 """
 
 from __future__ import division
 import sys,re,random,math,datetime,re,time
 sys.dont_write_bytecode = True
 
-f=open('output.log','w')
 rand=random.random
 randi=random.randint
 exp=math.exp
 
 class simulatedAnnealing:
-  def say(x): 
-    f.write(str(x)); sys.stdout.flush()
 
-  def energy(x):
+  def __init__(self):
+    pass
+  
+  def energy(self,x):
     return x**2+(x-2)**2
 
-  def neighbour(x, xmin, xmax):
+  def neighbour(self, x, xmin, xmax):
     x_new=x+xmin+(xmax-xmin)*rand()
     return x_new
 
-  def pAcceptance(e, en, t):
+  def pAcceptance(self, e, en, t):
     p=exp((e-en)/t)
     return p
  
+  f=open('output.log','w')
+  def say(self,x): 
+    self.f.write(str(x)); 
+    sys.stdout.flush()
+ 
+# Initial temperature
+ 
+class main:
+
+  k=1
+  kmax=2000
+  emax=40
+  xmax=100;
+  xmin=-100;
+  
+  sa=simulatedAnnealing()
+
   # Initial state and energy
   s=randi(-1000,1000)
-  e=energy(s)
+  e=sa.energy(s)
 
   # Initial best state and energy
   sb=s;
   eb=e;
-
-  k=1 # Initial temperature
-  kmax=2000
-  emax=40
-  say(sb)
-  xmax=100;
-  xmin=-100;
-  while (k<kmax): 
-    sn=neighbour(sb,xmax,xmin)
-    en=energy(sn)
+ 
+  sa.say(sb)
+  emin=2
+  
+  while (k<kmax and eb>emin): 
+    sn=sa.neighbour(sb,xmax,xmin)
+    en=sa.energy(sn)
     t=k/kmax
-  #  print t
+    #print eb
     if en<eb:
-      say('!')
+      sa.say('!')
       sb=sn
       eb=en
 
     if en<e:
       s=sn
       e=en
-      say('+')
+      sa.say('+')
 
-    elif pAcceptance(e, en, t)>rand():
+    elif sa.pAcceptance(e, en, t)>rand():
       s=sn
       e=en
-      say('?')
+      sa.say('?')
   
-    say('.')
+    sa.say('.')
     k=k+1
-    if k%50==0: say('\n'), say(sb)
-if __name__=='main':
+    if k%50==0: sa.say('\n'), sa.say(sb)
 
-  simulatedAnnealing()
+if __name__=='main':
+  main()
