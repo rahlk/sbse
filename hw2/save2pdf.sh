80ae7d5d7ae6b7ea52d4734853f3a641ed98c314
Rahul <i.m.ralk@gmail.com>
2014-09-09 09:14:30 -0400

    More changes..

--- hw2/save2pdf.sh
+++ hw2/save2pdf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 Title="csc710sbse: hw2:Rahul Krishna"
-a2ps --center-title="$Title" -qr2gC -o ~/tmp/listing.ps SA_Fonseca.py log_sa_fonseca.txt SA_Kursawe.py log_sa_kursawe.txt MaxWalkSat.py log_mwalksat.txt
+a2ps --center-title="$Title" -qr2gC -o ~/tmp/listing.ps SA_Fonseca.py log_sa_fonseca.txt SA_Kursawe.py log_sa_kursawe.txt MaxWalkSat.py log_mwalksat.txt writtenText.txt
 ps2pdf ~/tmp/listing.ps listing.pdf
 
