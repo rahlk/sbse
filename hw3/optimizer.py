e51e8a0a4c9b9169ebdc2892220cfc275ef06882
Rahul <i.m.ralk@gmail.com>
2014-09-16 13:23:46 -0400

    HW 3 Modified

--- hw3/optimizer.py
+++ hw3/optimizer.py
@@ -2,56 +2,32 @@ from __future__ import division
 from searcher import *
 from models import *
 import sys
+import numpy as np
+#from sk import *
 from time import gmtime, strftime
 import sys, random, math, datetime, time,re
 sys.dont_write_bytecode = True
 
-def xtile(lst,lo=0,hi=0.001,width=50,
-             chops=[0.1 ,0.3,0.5,0.7,0.9],
-             marks=["-" ," "," ","-"," "],
-             bar="|",star="*",show=" %3.0f"):
-  """The function _xtile_ takes a list of (possibly)
-  unsorted numbers and presents them as a horizontal
-  xtile chart (in ascii format). The default is a 
-  contracted _quintile_ that shows the 
-  10,30,50,70,90 breaks in the data (but this can be 
-  changed- see the optional flags of the function).
-  """
-  def pos(p)   : return ordered[int(len(lst)*p)]
-  def place(x) : 
-    return int(width*float((x - lo))/(hi - lo))
-  def pretty(lst) : 
-    return ', '.join([show % x for x in lst])
-  ordered = sorted(lst)
-  lo      = min(lo,ordered[0])
-  hi      = max(hi,ordered[-1])
-  what    = [pos(p)   for p in chops]
-  where   = [place(n) for n in  what]
-  out     = [" "] * width
-  for one,two in pairs(where):
-    for i in range(one,two): 
-      out[i] = marks[0]
-    marks = marks[1:]
-  out[int(width/2)]    = bar
-  out[place(pos(0.5))] = star 
-  return ''.join(out) +  "," +  pretty(what)
-  
 
 for x in [Schaffer, Kursawe, Fonseca, ZDT1]:
   eb=50*[None]
-  for y in [sa, MaxWalkSat]:
-    for r in xrange(1):
-      print '\n', x.__name__, y.__name__, '\n', strftime("%a, %d %b %Y %H:%M:%S ", gmtime())
+  for y in [SimulatedAnnealer, MaxWalkSat]:
+    print 'Model: ', x.__name__ 
+    print 'Searcher: ', y.__name__ 
+    print strftime("%a, %d %b %Y %H:%M:%S ", gmtime()), '\n'
+    k=x()
+    hi, lo, kooling, indepSize, iterations = k.getInit()
+    print 'Searcher settings:'
+    print 'min=', lo, ', max=', hi, ', Cooling Factor=', kooling, '\n'
+    for r in xrange(50):
       a=y(x,disp=False)
-      k=x()
-      hi, lo, kooling, indepSize, iterations = k.getInit()
-      print 'min=', lo, ', max=', hi, ', Cooling Factor=', kooling
       eb[r] =  a.runSearcher()
-  
-    #print eb/20  
+    #print xtile(eb)
+    print 'Energy: ', np.sum(eb)/50, 
+    print '\n'  
   for x in xrange(50): sys.stdout.write('-')
   sys.stdout.write('\n')
-  #xtile(eb)
+  
   
   
   
